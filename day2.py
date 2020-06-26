9fd516a597c17e55b23e62f9debe67720cb35b97
Nikki <nikmck22@gmail.com>
2020-06-26 16:42:12 -0400

    recursion examples

--- day2.py
+++ day2.py
@@ -8,8 +8,17 @@
 # print(count_vowels("hello"))
 
 
-def digit_sum(n):
-  if n == 0: return 0
-  return digit_sum(n//10) + n % 10
+# def digit_sum(n):
+#   if n == 0: return 0
+#   return digit_sum(n//10) + n % 10
 
-print(digit_sum(345))
+# print(digit_sum(345))
+
+
+def fib(n):
+  if n == 0 or n == 1:
+    return 1
+  return fib(n-2) + fib(n-1)
+
+for i in range(10):
+    print(fib(i))
