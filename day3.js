71fa9e5dc827cd3af8c70b4e4d0e7e2603eb3850
Nikki <nikmck22@gmail.com>
2020-06-27 07:19:20 -0400

    add notes doc

--- day3.js
+++ day3.js
@@ -4,3 +4,11 @@
  Create 2 variables related to your dog. Make one of them its age and update it by one.
  Log out all the values in a friendly way of your choosing.
 */
+
+let name = 'diesel';
+let age = 2;
+age = age + 1;
+
+
+
+console.log('my dog ' + name + ' is turning ' + age)
