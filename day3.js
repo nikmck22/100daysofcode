fcc5f7de19f05978b95b5beb1a55f808ff928fbc
Nikki <nikmck22@gmail.com>
2020-06-27 07:10:30 -0400

    day3 initial commit

---
+++ day3.js
@@ -0,0 +1,6 @@
+// Challenge: It your Dog's Birthday!
+
+/*
+ Create 2 variables related to your dog. Make one of them its age and update it by one.
+ Log out all the values in a friendly way of your choosing.
+*/
