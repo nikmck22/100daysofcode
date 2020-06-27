fa69d52bfeb5950c54f16ab7d8f7a893904c7484
Nikki <nikmck22@gmail.com>
2020-06-27 07:33:07 -0400

    types of variables, interp, concat

--- day3.js
+++ day3.js
@@ -5,10 +5,19 @@
  Log out all the values in a friendly way of your choosing.
 */
 
-let name = 'diesel';
-let age = 2;
-age = age + 1;
+// const name = 'diesel';
+// let age = 2;
+// age = age + 1;
 
 
 
-console.log('my dog ' + name + ' is turning ' + age)
+// console.log('my dog ' + name + ' is turning ' + age)
+
+
+
+
+let firstName = 'Harry';
+let lastName = 'Potter';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName)
