37e7916ad95bafdba218198e49d8cbbbafd07ba0
Nikki <nikmck22@gmail.com>
2020-06-28 07:17:00 -0400

    if statements

--- day4.js
+++ day4.js
@@ -1,16 +1,40 @@
-// Challenge: Upsell Quantity
+// Challenge 1: User Age
 
 /*
- 1. Our business wants to upsell items in a user's cart. 
- 2. Ask the user if they would like to add one more item (with the variable provided). Update the quantity variable if they do.
- 3. Log the current quantity.
+ 1. Our business wants to keep track of what age range a user falls into.
+ 2. If they are 12 or younger log 'child'.
+ 3. If not a child but the user's age is under 20 log 'teenager'.
+ 4. If neither log 'adult'.
 */
-const customerAnswer = Math.random() >= 0.5;
-let quantity = 2;
 
-if (customerAnswer) {
-  quantity++;
+const age = 10;
+
+if (age > 12 && age < 20) {
+  console.log('teenager');
+} else if (age <= 12) {
+  console.log('child');
+} else {
+  console.log('adult');
 }
 
+// OR see below
+// if(age <= 12) {
+//   console.log('child');
+// } else if(age < 20) {
+//   console.log('teenager');
+// } else {
+//   console.log('adult');
+// }
 
-console.log(quantity)
+
+// Challenge 2: Practice and or Operators
+
+/*
+    1. Look at the examples below and see if you can guess the 
+*/
+
+// console.log(10 === 10 && 5 < 4); false
+
+// console.log(10 === 10 || 5 < 4); true
+
+// console.log((5 >= 5 || 4 > 4) && 3 + 2 === 5); true
