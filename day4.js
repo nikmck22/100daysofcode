beda0d9aa3213f52633d6cc7b84d03fad3d81fdd
Nikki <nikmck22@gmail.com>
2020-06-28 07:02:30 -0400

    relational operators

--- day4.js
+++ day4.js
@@ -1,21 +1,12 @@
-// Challenge: Checkout Order Details
+// Challenge: Best Promo Code
 
 /*
- 1. Create 3 variables to store an item price. The last price 2 items were ordered (multiplication).
- 2. Tell the user what the total price is before discount.
- 3. Inform the user what the average price of items is before discount.
- 4. Inform them the final price after saving 5 dollars with promo code 'I Got 5 On It'.
+ 1. Our business wants to make sure we give our users the best promo codes available for two categories.
+ 2. Create two promo codes in variables and compare to find out if the first one is a better value for the dollars off category. 
 */
 
-const price1 = 5;
-const price2 = 3;
-const price3 = 7;
 
-const totalPrice = price1 + price2 + (price3 * 2);
+const promo1 = 10;
+const promo2 = 15;
 
-const avgPrice = totalPrice / 4;
-
-const discountPrice = totalPrice - 5;
-
-
-console.log(totalPrice, avgPrice, discountPrice)
+console.log(promo1 < promo2)
