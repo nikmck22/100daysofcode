8589083cc78a0c9e6e6c3404b34c6599593a3bd0
Nikki <nikmck22@gmail.com>
2020-07-04 09:32:45 -0400

    working in arrays

---
+++ day10.js
@@ -0,0 +1,42 @@
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+var myArray = ["dog", 6];
+
+// Create a nested array called myArray.
+var myArray = [["dog", "cat"], ["fish", "bird"]];
+
+
+// Create a variable called myData and set it to equal the first value of myArray using bracket notation.
+var myArray = [50,60,70];
+var myData = myArray[0];
+
+
+// Modify the data stored at index 0 of myArray to a value of 45.
+var myArray = [18,64,99];
+myArray[0] = 45;
+
+
+// Using bracket notation select an element from myArray such that myData is equal to 8.
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+var myData = myArray[2][1];
+
+
+// Push ["dog", 3] onto the end of the myArray variable.
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+
+
+// Use the .pop() function to remove the last item from myArray, assigning the "popped off" value to removedFromMyArray.
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop();
+
+
+// Use the .shift() function to remove the first item from myArray, assigning the "shifted off" value to removedFromMyArray.
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.shift();
+
+
+// Add ["Paul",35] to the beginning of the myArray variable using unshift().
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+
