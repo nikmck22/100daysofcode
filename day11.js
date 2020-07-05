64eaa21e846aac681f3d7b98e9949d39d3c3843e
Nikki <nikmck22@gmail.com>
2020-07-05 18:51:34 -0400

    include golf challenge

--- day11.js
+++ day11.js
@@ -81,3 +81,91 @@ function testLogicalOr(val) {
   return "Inside";
 }
 testLogicalOr(15);
+
+
+// Combine the if statements into a single if/else statement.
+function testElse(val) {
+  var result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+  else {
+    result = "5 or Smaller";
+  }
+  return result;
+}
+testElse(4);
+
+
+// Convert the logic to use else if statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+testElseIf(7);
+
+
+// Change the order of logic in the function so that it will return the correct statements in all cases.
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
+
+
+// Write chained if/else if statements to fulfill the following conditions:
+// num < 5 - return "Tiny"
+// num < 10 - return "Small"
+// num < 15 - return "Medium"
+// num < 20 - return "Large"
+// num >= 20 - return "Huge"
+function testSize(num) {
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  } else {
+    return "Change Me";
+  }
+}
+testSize(7);
+
+
+// Golf score exercise:
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (par == strokes) {
+    return "Par";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes >= par + 3) {
+    return "Go Home!";
+  } else {
+    return "Change Me";
+  }
+}
+golfScore(5, 4);
