d21a5c6dd54dd53c00123824fabd62919ac991b6
Nikki <nikmck22@gmail.com>
2020-07-05 18:24:48 -0400

    else statements

--- day11.js
+++ day11.js
@@ -35,3 +35,49 @@ function testNotEqual(val) {
   return "Equal";
 }
 testNotEqual(10);
+
+
+// Add the greater than operator to the indicated lines so that the return statements make sense.
+function testGreaterThan(val) {
+  if (val > 100) {  
+    return "Over 100";
+  }
+  if (val > 10) {  
+    return "Over 10";
+  }
+  return "10 or Under";
+}
+testGreaterThan(10);
+
+
+// Add the less than operator to the indicated lines so that the return statements make sense.
+function testLessThan(val) {
+  if (val < 25) {  
+    return "Under 25";
+  }
+  if (val < 55) {  
+    return "Under 55";
+  }
+  return "55 or Over";
+}
+testLessThan(10);
+
+
+// Replace the two if statements with one statement, using the && operator, which will return "Yes" if val is less than or equal to 50 and greater than or equal to 25. Otherwise, will return "No".
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+  return "No";
+}
+testLogicalAnd(10);
+
+
+// Combine the two if statements into one statement which returns "Outside" if val is not between 10 and 20, inclusive. Otherwise, return "Inside".
+function testLogicalOr(val) {
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+  return "Inside";
+}
+testLogicalOr(15);
