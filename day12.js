f51b6ee49301afbfda5994c0875e04783a1e4ab1
Nikki <nikmck22@gmail.com>
2020-07-06 19:09:38 -0400

    return boolean from function

--- day12.js
+++ day12.js
@@ -22,3 +22,64 @@ switch(val) {
   return answer;
 }
 caseInSwitch(1);
+
+
+// Write a switch statement to set answer for the following conditions:
+// "a" - "apple"
+// "b" - "bird"
+// "c" - "cat"
+// default - "stuff"
+function switchOfStuff(val) {
+  var answer = "";
+switch (val) {
+  case "a":
+    answer = "apple";
+    break;
+  case "b":
+    answer = "bird";
+    break;
+  case "c":
+    answer = "cat";
+    break;
+  default:
+    answer = "stuff";
+    break;
+}
+  return answer;
+}
+switchOfStuff(1);
+
+
+// Write a switch statement to set answer for the following ranges:
+// 1-3 - "Low"
+// 4-6 - "Mid"
+// 7-9 - "High"
+function sequentialSizes(val) {
+  var answer = "";
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    answer = "Low";
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = "Mid";
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = "High";
+    break;
+}
+  return answer;
+}
+sequentialSizes(1);
+
+
+// Fix the function isLess to remove the if/else statements.
+function isLess(a, b) {
+return (a < b);
+}
+isLess(10, 15);
