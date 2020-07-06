23964613a1d8e18ca879f8bb7e1513fd9572e909
Nikki <nikmck22@gmail.com>
2020-07-06 19:30:27 -0400

    black jack challenge

--- day12.js
+++ day12.js
@@ -83,3 +83,34 @@ function isLess(a, b) {
 return (a < b);
 }
 isLess(10, 15);
+
+
+
+// You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+var count = 0;
+function cc(card) {
+  switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 10:
+  case "J":
+  case "Q":
+  case "K":
+  case "A":
+    count--;
+    break;
+  }
+  if (count > 0) {
+    return count + " Bet"
+  } else {
+    return count + " Hold"
+  }
+  return "Change Me";
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
