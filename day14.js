43fab7730fe9ac386d7fe456f089688f58be15b3
Nikki <nikmck22@gmail.com>
2020-07-08 21:05:04 -0400

    running loops

--- day14.js
+++ day14.js
@@ -87,3 +87,13 @@ else if (prop == "tracks") {
 }
 
 updateRecords(5439, "artist", "ABBA");
+
+
+
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+var myArray = [];
+var i = 5;
+while(i > -1) {
+  myArray.push(i);
+  i--;
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+var myArray = [];
+var i = 5;
+while(i > -1) {
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray)
