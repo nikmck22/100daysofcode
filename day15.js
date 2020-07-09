2ab99365196aa3270c474cafaa50c390466081af
Nikki <nikmck22@gmail.com>
2020-07-09 17:24:16 -0400

    basic js complete

--- day15.js
+++ day15.js
@@ -6,3 +6,32 @@ if (n <= 0) {
   return sum(arr, n - 1) + arr[n - 1]
 }
 }
+
+
+
+// Change randomFraction to return a random number instead of returning 0.
+function randomFraction() {
+  return Math.random();
+}
+
+
+
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
+
+//with radix
+function convertToInteger(str) {
+  return parseInt(str, 2);
+  }
+  convertToInteger("10011");
+
+
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either "Equal" or "Not Equal".
+function checkEqual(a, b) {
+  return a == b ? "Equal" : "Not Equal"
+  }
+  checkEqual(1, 2);
