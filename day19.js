548f642316423520f31b6424bc454a3bf9b13a1d
Nikki <nikmck22@gmail.com>
2020-07-13 18:41:59 -0400

    fulfill promise with result

--- day19.js
+++ day19.js
@@ -7,3 +7,21 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject("Data not received");
   }
 });
+
+
+
+// Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
+const makeServerRequest = new Promise((resolve, reject) => {
+  let responseFromServer = true;
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+makeServerRequest.then(result => {
+  console.log(result)
+})
+
+
+// Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.
