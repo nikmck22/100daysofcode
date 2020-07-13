4d11e5cd34c931ce059a145018cd3a8ebcdad93c
Nikki <nikmck22@gmail.com>
2020-07-12 21:04:32 -0400

    JS promises

--- day18.js
+++ day18.js
@@ -37,3 +37,8 @@ const bicycle = {
 };
 
 
+
+
+// Create a new promise called makeServerRequest. Pass in a function with resolve and reject parameters to the constructor.
+const makeServerRequest = new Promise((resolve, reject) => {
+}); 
