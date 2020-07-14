ffb33322f58d0009bfa1c2e39abf2df3c8a818f8
Nikki <nikmck22@gmail.com>
2020-07-14 19:40:24 -0400

    numbers and letters

--- day20.js
+++ day20.js
@@ -9,3 +9,32 @@ let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/; 
 let result = extractStr.match(/coding/); 
 
+
+// Using the regex starRegex, find and extract both "Twinkle" words from the string twinkleStar.
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; 
+let result = twinkleStar.match(/twinkle/gi); 
+
+
+// Complete the regex unRegex so that it matches the strings "run", "sun", "fun", "pun", "nun", and "bun". Your regex should use the wildcard character.
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; 
+let result = unRegex.test(exampleStr);
+
+
+// Use a character class with vowels (a, e, i, o, u) in your regex vowelRegex to find all the vowels in the string quoteSample.
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; 
+let result = quoteSample.match(/[aeiou]/gi); 
+
+
+// Match all the letters in the string quoteSample.
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(/[a-z]/gi); 
+
+
+// Create a single regex that matches a range of letters between h and s, and a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; 
+let result = quoteSample.match(/[h-s2-6]/gi) 
