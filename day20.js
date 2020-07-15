a1b58fac6fafe483a224f1aa9e68ac95a180f95f
Nikki <nikmck22@gmail.com>
2020-07-14 20:00:36 -0400

    whitespace and shorthand

--- day20.js
+++ day20.js
@@ -38,3 +38,17 @@ let result = quoteSample.match(/[a-z]/gi);
 let quoteSample = "Blueberry 3.141592653s are delicious.";
 let myRegex = /[h-s2-6]/gi; 
 let result = quoteSample.match(/[h-s2-6]/gi) 
+
+
+// You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+// 1) Usernames can only use alpha-numeric characters.
+
+// 2) The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+// 3) Username letters can be lowercase and uppercase.
+
+// 4) Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+
+// Change the regex userCheck to fit the constraints listed above.
+/^[a-z]([0-9][0-9]+|[a-z]\d*)$/i
