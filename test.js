46e1075110de4cf08cb53e3a4ab09a7a7bee2234
Nikki <nikmck22@gmail.com>
2020-07-14 20:33:43 -0400

    complete regex exercises

--- test.js
+++ test.js
@@ -1,11 +1,5 @@
-function multiplyAll(arr) {
-  var product = 1;
-for (var i = 0; i < arr.length; i++) {
-  for (var j = 0; j < arr[i].length; j++) {
-    product = arr[i] * arr[j];
-  }
-}
-  return product;
-}
+let hello = "   Hello, World!  ";
+let wsRegex = /^(?=[a-z])/; 
+let result = hello;
 
-multiplyAll([[1,2],[3,4],[5,6,7]]);
+console.log(result)
