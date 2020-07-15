a05e7c803dcc9c1e9d7b5d225555ae0d72465f34
Nikki <nikmck22@gmail.com>
2020-07-15 16:50:14 -0400

    catching syntax errors

---
+++ day21.js
@@ -0,0 +1,10 @@
+//finding the type of a variable
+let a = 7;
+let b = "3";
+console.log(typeof a);
+console.log(typeof b);
+
+
+let myArray = [1, 2, 3];
+let arraySum = myArray.reduce((previous, current) =>  previous + current);
+console.log(`Sum of array values is: ${arraySum}`);
