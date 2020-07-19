0b8baa7a48f2daab624e82db23011d4e1a5e8033
Nikki <nikmck22@gmail.com>
2020-07-19 10:09:38 -0400

    longest word in array

--- day25.js
+++ day25.js
@@ -30,3 +30,20 @@ function factorialize(num) {
 }
 
 console.log(factorialize(5));
+
+
+
+//find the longest word in the array (return number)
+function findLongestWordLength(str) {
+  let arr = str.split(" ");
+  let maxLength = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > maxLength) {
+      maxLength = arr[i].length;
+    }
+  }
+  return maxLength;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
