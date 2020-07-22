ed6377def99fb24fe94432f8a6762a521da3514e
Nikki <nikmck22@gmail.com>
2020-07-22 14:34:13 -0400

    boolean primitive

--- day27.js
+++ day27.js
@@ -23,3 +23,13 @@ function findElement(arr, func) {
 }
 
 console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+
+
+
+//boolean primitive
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
+
+booWho(null);
+
--- test.js
+++ test.js
@@ -1,12 +1,5 @@
-function findElement(arr, func) {
-  let num = 0;
-  for (var i = 0; i < arr.length; i++) {
-    num = arr[i];
-    if (func(num)) {
-      return num;
-    }
-  }
-  return undefined;
+function booWho(bool) {
+  return typeof bool === "boolean";
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+console.log(booWho(false));
