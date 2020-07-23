a170412f4edcc9885392ce8e3bd09c3880273ff8
Nikki <nikmck22@gmail.com>
2020-07-23 17:21:34 -0400

    slice and splice

--- day28.js
+++ day28.js
@@ -8,3 +8,17 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"));
+
+
+
+//slice and splice
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return newArr;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-//title case a sentence
-function titleCase(str) {
-  var sentence = str.toLowerCase().split(" ");
-  for (let i = 0; i < sentence.length; i++) {
-    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice();
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.splice(n, 0, arr1[i]);
+    n++;
   }
-  return sentence.join(" ");
+  return newArr;
 }
 
-console.log(titleCase("I'm a little tea pot"));
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
