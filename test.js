b807397596e447ec1ecfd040a1fee40618c64290
Nikki <nikmck22@gmail.com>
2020-07-23 16:43:01 -0400

    title case a string parameter

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 //title case a sentence
 function titleCase(str) {
-  str.slice(" ")
-  return str;
+  var sentence = str.toLowerCase().split(" ");
+  for (let i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  }
+  return sentence.join(" ");
 }
 
 console.log(titleCase("I'm a little tea pot"));
