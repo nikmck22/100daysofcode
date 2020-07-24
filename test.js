ddd6b3ffce9c642624d52b430d1ff0c1c7202630
Nikki <nikmck22@gmail.com>
2020-07-24 14:19:19 -0400

    day 29 initial commit

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-function frankenSplice(arr1, arr2, n) {
-  let newArr = arr2.slice();
-  for (let i = 0; i < arr1.length; i++) {
-    newArr.splice(n, 0, arr1[i]);
-    n++;
+function bouncer(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    
   }
-  return newArr;
+  // return arr;
 }
 
-console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+console.log(bouncer([7, "ate", "", false, 9]));
