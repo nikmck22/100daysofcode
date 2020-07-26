8602646b8c065cc46402b4eef74c218f35623919
Nikki <nikmck22@gmail.com>
2020-07-25 20:48:51 -0400

    two dimensional arrays

--- day30.js
+++ day30.js
@@ -10,3 +10,16 @@ function mutation(arr) {
 }
 
 console.log(mutation(["hELLo", "HEY"]));
+
+
+
+//splitting arrays into two-dimensional arrays
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-function mutation(arr) {
-  let word1 = arr[0].toLowerCase();
-  let word2 = arr[1].toLowerCase();
-  
-  for (let i = 0; i < word2.length; i++) {
-    if (word1.indexOf(word2[i]) < 0) return false;
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
   }
-  return true;
+  return newArr;
 }
 
-console.log(mutation(["hELLo", "HEY"]));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
