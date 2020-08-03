5b00e3a3650c714326bb9cea6fdebd06a68ccd0e
Nikki <nikmck22@gmail.com>
2020-08-03 16:42:25 -0400

    using concat in arrays

--- day36.js
+++ day36.js
@@ -5,3 +5,13 @@ function nonMutatingConcat(original, attach) {
 var first = [1, 2, 3];
 var second = [4, 5];
 console.log(nonMutatingConcat(first, second));
+
+
+
+// Change the nonMutatingPush function so it uses concat to add newItem to the end of original instead of push. The function should return an array.
+function nonMutatingPush(original, newItem) {
+  return original.concat(newItem);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+console.log(nonMutatingPush(first, second));
--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-function nonMutatingConcat(original, attach) {
+function nonMutatingPush(original, newItem) {
   // Only change code below this line
-return original.concat(attach)
+  return original.concat(newItem);
 
   // Only change code above this line
 }
 var first = [1, 2, 3];
 var second = [4, 5];
-console.log(nonMutatingConcat(first, second));
+console.log(nonMutatingPush(first, second));
