57b2b9ed6dbeded31cc99bf30cbdffd916e16163
Nikki <nikmck22@gmail.com>
2020-08-04 17:55:14 -0400

    currying and partial application

--- day37.js
+++ day37.js
@@ -1,8 +1,8 @@
 //squareList function so that it returns a new array containing only the square of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it.
 const squareList = (arr) => {
-  // Only change code below this line
+  
   return arr.filter(num => num > 0 && num % parseInt(num) === 0).map(num => Math.pow(num, 2));
-  // Only change code above this line
+  
 };
 
 const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
@@ -12,12 +12,12 @@ console.log(squaredIntegers);
 
 // Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
 function alphabeticalOrder(arr) {
-  // Only change code below this line
+  
 return arr.sort(function(a, b) {
   return a === b ? 0 : a > b ? 1 : -1;
 });
 
-  // Only change code above this line
+  
 }
 console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
 
@@ -26,13 +26,13 @@ console.log(alphabeticalOrder(["a", "d", "c", "a", "z", "g"]));
 //return sorted array without changing original
 var globalArray = [5, 6, 3, 2, 9];
 function nonMutatingSort(arr) {
-  // Only change code below this line
+  
   arr = globalArray.slice(0);
 return arr.sort(function(a, b) {
   return a - b;
 });
 
-  // Only change code above this line
+  
 }
 console.log(nonMutatingSort(globalArray));
 
@@ -41,10 +41,10 @@ console.log(nonMutatingSort(globalArray));
 //regular expression - remove non-word characters
 
 function splitify(str) {
-  // Only change code below this line
+  
 return str.split(/\W/);
 
-  // Only change code above this line
+  
 }
 console.log(splitify("Hello World,I-am code"));
 
@@ -52,10 +52,10 @@ console.log(splitify("Hello World,I-am code"));
 
 //Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, "I-like-Star-Wars" would be converted to "I like Star Wars". For this challenge, do not use the replace method.
 function sentensify(str) {
-  // Only change code below this line
+  
 return str.split(/\W/).join(" ");
 
-  // Only change code above this line
+  
 }
 console.log(sentensify("May-the-force-be-with-you"));
 
@@ -66,7 +66,7 @@ console.log(sentensify("May-the-force-be-with-you"));
 // The global variable
 var globalTitle = "Winter Is Coming";
 
-// Only change code below this line
+
 function urlSlug(title) {
 return title
 .split(/\W/)
@@ -77,7 +77,7 @@ return title
 .toLowerCase();
 
 }
-// Only change code above this line
+
 console.log(urlSlug(globalTitle))
 
 
@@ -85,22 +85,38 @@ console.log(urlSlug(globalTitle))
 
 //every method
 function checkPositive(arr) {
-  // Only change code below this line
+  
 return arr.every(function(value) {
   return value > 0;
 })
 
-  // Only change code above this line
+  
 }
 console.log(checkPositive([1, 2, 3, -4, 5]));
 
 //some method
 function checkPositive(arr) {
-  // Only change code below this line
+  
 return arr.some(function(value) {
   return value > 0;
 })
 
-  // Only change code above this line
+  
 }
 console.log(checkPositive([1, 2, 3, -4, 5]));
+
+
+
+
+//currying and partial application
+function add(x) {
+  
+return function(y) {
+  return function(z) {
+    return x + y + z
+  }
+}
+
+  
+}
+console.log(add(10)(20)(30));
