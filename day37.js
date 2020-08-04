23432d5101360b354c389edcf7561ede5109c732
Nikki <nikmck22@gmail.com>
2020-08-04 17:06:24 -0400

    square list function

--- day37.js
+++ day37.js
@@ -1,1 +1,9 @@
+//squareList function so that it returns a new array containing only the square of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it.
+const squareList = (arr) => {
+  // Only change code below this line
+  return arr.filter(num => num > 0 && num % parseInt(num) === 0).map(num => Math.pow(num, 2));
+  // Only change code above this line
+};
 
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
