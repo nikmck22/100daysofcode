2fdf36c99a59b8e4ddeb24d265e2b0beafe1bb95
Nikki <nikmck22@gmail.com>
2020-08-11 18:20:44 -0400

    destroyer solution

---
+++ day42.js
@@ -0,0 +1,10 @@
+function destroyer(arr) {
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
+}
+
+// Declare a variable named args and set it equal to a new Array object from() the arguments passed into the function. On the same or next line, use the slice() method on args starting from the second index, 1. This separates the arguments used for filtering into their own array of args.
+
+// Return the filtered array, using includes() in the callback function to check if val is not in args; returning true to keep the value in the original array or false to remove it.
--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-function diffArray(arr1, arr2) {
-  var newArr = arr1.concat(arr2);
-  return newArr.filter(item => !arr1.includes(item) || !arr2.includes(item));
-  
+function destroyer(arr) {
+  for 
 }
 
-console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
