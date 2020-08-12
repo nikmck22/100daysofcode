02d9de68a53b1063de2bd29ff026eb2635279fb2
Nikki <nikmck22@gmail.com>
2020-08-12 18:32:01 -0400

    day 43 initial commit

--- day42.js
+++ day42.js
@@ -4,7 +4,7 @@ function destroyer(arr) {
     return !args.includes(val);
   });
 }
-
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 // Declare a variable named args and set it equal to a new Array object from() the arguments passed into the function. On the same or next line, use the slice() method on args starting from the second index, 1. This separates the arguments used for filtering into their own array of args.
 
 // Return the filtered array, using includes() in the callback function to check if val is not in args; returning true to keep the value in the original array or false to remove it.
