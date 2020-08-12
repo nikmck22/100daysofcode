3bc4e3e8b57a9db58549dbf97355eec58bb88b10
Nikki <nikmck22@gmail.com>
2020-08-12 18:56:22 -0400

    function looks through array of obj

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
-function destroyer(arr) {
-  for 
+function whatIsInAName(collection, source) {
+  var arr = [];
+  // Only change code below this line
+ var srcKeys = Object.keys(source);
+ return collection.filter(function(obj) {
+   for (var i = 0; i < srcKeys.length; i++) {
+     if (!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+       return false;
+     }
+     } 
+     return true;
+   
+ });
+  // Only change code above this line
+ 
 }
 
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
