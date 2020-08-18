873eefb0b3cf37ecb334871aeafc0e8757fdc837
Nikki <nikmck22@gmail.com>
2020-08-17 22:09:19 -0400

    pig latin

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 
 
 function translatePigLatin(str) {
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  let newStr = "";
+  let regEx = /[aeiou]/gi;
+  let newStr = str.toLowerCase();
+  let charIndex;
+
+  if (newStr[0].match(regEx)) {
+    return newStr + "way";
+  }
+  
+  for (char of newStr) {
+    if (char.match(regEx)) {
+      charIndex = newStr.indexOf(char);
+      break;
+    }
+  }
+  return newStr.slice(charIndex) + newStr.slice(0, charIndex) + "ay";
 }
 
 console.log(translatePigLatin("consonant"));
