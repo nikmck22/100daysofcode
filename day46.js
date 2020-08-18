a53b63c86e8354a761963281ae93f8fc737af2e7
Nikki <nikmck22@gmail.com>
2020-08-18 18:31:40 -0400

    DNA base pairs

--- day46.js
+++ day46.js
@@ -11,3 +11,31 @@ function myReplace(str, before, after) {
 }
 
 console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
+
+
+//DNA base pairs
+function pairElement(str) {
+  var newArr = [];
+  var search = function(char) {
+    switch (char) {
+      case "A":
+        newArr.push(["A", "T"]);
+        break;  
+      case "T":
+        newArr.push(["T", "A"]);
+        break;
+      case "C":
+        newArr.push(["C", "G"]);
+        break;
+      case "G":
+        newArr.push(["G", "C"]);
+        break;
+    }
+  };
+  for (var i = 0; i < str.length; i++) {
+    search(str[i])
+  }
+  return newArr;
+}
+
+console.log(pairElement("GCG"));
--- test.js
+++ test.js
@@ -1,13 +1,25 @@
-function myReplace(str, before, after) {
-  var index = str.indexOf(before);
-  if (str[index] === str[index].toUpperCase()) {
-    after = after.charAt(0).toUpperCase() + after.slice(1);
-  } else {
-    after = after.charAt(0).toLowerCase() + after.slice(1);
+function pairElement(str) {
+  var newArr = [];
+  var search = function(char) {
+    switch (char) {
+      case "A":
+        newArr.push(["A", "T"]);
+        break;  
+      case "T":
+        newArr.push(["T", "A"]);
+        break;
+      case "C":
+        newArr.push(["C", "G"]);
+        break;
+      case "G":
+        newArr.push(["G", "C"]);
+        break;
+    }
+  };
+  for (var i = 0; i < str.length; i++) {
+    search(str[i])
   }
-  str = str.replace(before, after);
-  return str;
+  return newArr;
 }
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
-
+console.log(pairElement("GCG"));
