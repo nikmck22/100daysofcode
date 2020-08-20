b78011d34548faf7cba039689fe6c4b9f8fb7df0
Nikki <nikmck22@gmail.com>
2020-08-19 20:25:58 -0400

    day 47 initial commit

--- test.js
+++ test.js
@@ -1,25 +1,5 @@
-function pairElement(str) {
-  var newArr = [];
-  var search = function(char) {
-    switch (char) {
-      case "A":
-        newArr.push(["A", "T"]);
-        break;  
-      case "T":
-        newArr.push(["T", "A"]);
-        break;
-      case "C":
-        newArr.push(["C", "G"]);
-        break;
-      case "G":
-        newArr.push(["G", "C"]);
-        break;
-    }
-  };
-  for (var i = 0; i < str.length; i++) {
-    search(str[i])
-  }
-  return newArr;
+function fearNotLetter(str) {
+  return str;
 }
 
-console.log(pairElement("GCG"));
+console.log(fearNotLetter("abce"));
