9c209d936a68428470bc5f301fade64226edda7f
Nikki <nikmck22@gmail.com>
2020-08-19 21:04:11 -0400

    unique entities

--- day47.js
+++ day47.js
@@ -10,3 +10,21 @@ function fearNotLetter(str) {
 }
 
 console.log(fearNotLetter("abce"));
+
+
+//unique values
+function uniteUnique(arr) {
+  var newArr = [];
+  for (var i = 0; i < arguments.length; i++) {
+    var temp = arguments[i];
+    for (var g = 0; g < temp.length; g++) {
+      var temp2 = temp[g];
+      if (newArr.indexOf(temp2) < 0) {
+        newArr.push(temp2);
+      }  
+    }
+  }
+  return newArr;
+}
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-function fearNotLetter(str) {
-  for (var i = 0; i < str.length; i++) {
-    var code = str.charCodeAt(i);
-    if (code !== str.charCodeAt(0) + i) {
-      return String.fromCharCode(code - 1);
+function uniteUnique(arr) {
+  var newArr = [];
+  for (var i = 0; i < arguments.length; i++) {
+    var temp = arguments[i];
+    for (var g = 0; g < temp.length; g++) {
+      var temp2 = temp[g];
+      if (newArr.indexOf(temp2) < 0) {
+        newArr.push(temp2);
+      }  
     }
   }
-  return undefined;
+  return newArr;
 }
 
-console.log(fearNotLetter("abce"));
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
