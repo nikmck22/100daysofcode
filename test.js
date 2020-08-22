7771c54b3a1bde4e81e528fec6b061a98489f513
Nikki <nikmck22@gmail.com>
2020-08-22 18:42:37 -0400

    prime number sum

--- test.js
+++ test.js
@@ -1,28 +1,20 @@
-function sumFibs(num) {
-  var a = 1, b = 0, c;
-  var arr = [];
-  var num2 = num;
-  while (num2 >= 0) {
-    c = a;
-    a = a + b;
-    b = c;
-    if (b % 2 !== 0 && b <= num) {
-      arr.push(b); 
+function sumPrimes(num) {
+  let i = 1;
+  let sum = 0;
+  while (i <= num) {
+    if (isPrime(i)) {
+      sum += i;
     }
-    num2--;
+    i++;
   }
-  var arr2 = [];
-  for (var j = 0; j < arr.length; j++) {
-    if (arr[j] <= num) {
-      arr2.push(arr[j])
-    }
-  }
-  var sum = arr.reduce(function(a, b) {
-    return a + b;
-  }, 0);
   return sum;
 }
+//function to check if a number is prime or not
+function isPrime(x) {
+  for (let i = 2; i < x; i++) {
+    if (x % i === 0) return false;
+  }
+  return x !== 1 && x !== 0;
+}
 
-
-console.log(sumFibs(4));
-
+console.log(sumPrimes(10))
