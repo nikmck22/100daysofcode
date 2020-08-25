3e1c49f4e204ba5b8e60f9df1a05ee8812af0b1f
Nikki <nikmck22@gmail.com>
2020-08-25 19:11:22 -0400

    day 49 initial commit

---
+++ day49.js
@@ -0,0 +1,1 @@
+// Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
--- test.js
+++ test.js
@@ -1,20 +1,6 @@
-function sumPrimes(num) {
-  let i = 1;
-  let sum = 0;
-  while (i <= num) {
-    if (isPrime(i)) {
-      sum += i;
-    }
-    i++;
-  }
-  return sum;
-}
-//function to check if a number is prime or not
-function isPrime(x) {
-  for (let i = 2; i < x; i++) {
-    if (x % i === 0) return false;
-  }
-  return x !== 1 && x !== 0;
+function smallestCommons(arr) {
+  return arr;
 }
 
-console.log(sumPrimes(10))
+
+smallestCommons([1,5]);
