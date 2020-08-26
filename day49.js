d58ba9f8d6aa47d4ce3af31a9eac13de83e57c28
Nikki <nikmck22@gmail.com>
2020-08-25 20:08:52 -0400

    binary to text

--- day49.js
+++ day49.js
@@ -59,3 +59,16 @@ function steamrollArray(arr) {
 }
 
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
+
+
+
+//convert binary to text
+function binaryAgent(str) {
+  var binary = str.split(' ');
+  var output = binary
+  .map(bin => String.fromCharCode(parseInt(bin,2)))
+  .join('');
+  return output;
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
