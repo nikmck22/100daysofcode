b72ab9808c41c1776c43468b9d9683c2db49e468
Nikki <nikmck22@gmail.com>
2020-08-26 19:23:17 -0400

    whiteboard challenge 1

---
+++ day50.js
@@ -0,0 +1,11 @@
+//Whiteboard Challenge #1
+//duplicates in an array
+var containsDuplicate = function(nums) {
+  for (var i = 0; i < nums.length; i++) {
+      for (var j = i + 1; j < nums.length; j++) {
+          if (nums[i] === nums[j]) {
+              return true
+          } 
+      }
+  } return false
+};
