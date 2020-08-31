28da1cf5788632938ce960f8706b0ba1330d62c7
Nikki <nikmck22@gmail.com>
2020-08-31 17:36:15 -0400

    truthy elements

---
+++ day51.js
@@ -0,0 +1,8 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+function truthCheck(collection, pre) {
+  return collection.every(function(item) {
+    return item.hasOwnProperty(pre) && Boolean(item[pre]);
+  });
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa"}, {"user": "Po", "sex": "female"}], "sex"));
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 function truthCheck(collection, pre) {
-  return pre;
+  return collection.every(function(item) {
+    return item.hasOwnProperty(pre) && Boolean(item[pre]);
+  });
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa"}, {"user": "Po", "sex": "female"}], "sex"));
